b6f83e1e19a4b4c240bd857350b7e9d5f174a553
Diego Navarro <diego.nava07@gmail.com>
2014-09-11 09:09:43 -0500

    Local configuration, paquetes para env=local
    Configuración del local environment para todo el mundo, ademas de los
    paquetes para trabajo local

--- bootstrap/compiled.php
+++ bootstrap/compiled.php
@@ -477,7 +477,7 @@ class Application extends Container implements HttpKernelInterface, TerminableIn
     }
     public static function getBootstrapFile()
     {
-        return '/Users/jairo/Webs/HPCFront/vendor/laravel/framework/src/Illuminate/Foundation' . '/start.php';
+        return '/Users/perseus/Workspace/Apps/HPCFront/vendor/laravel/framework/src/Illuminate/Foundation' . '/start.php';
     }
     public function startExceptionHandling()
     {
