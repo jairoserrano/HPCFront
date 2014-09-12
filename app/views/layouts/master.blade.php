37c1149d3887ea4c57e5131bd116d3c9068a20de
Diego Navarro <diego.nava07@gmail.com>
2014-09-12 16:24:51 -0500

    Cambios de estilos
    Se han agregado un archivo css para poder sobreescribir los estilos de
    UTBootstrap y se encapsulo el top menu en un solo archivo para no
    cargar el master

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -21,54 +21,20 @@
     <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}">
 
     {{--Le styles--}}
-    {{ HTML::style('assets/bootstrap/dist/css/bootstrap.min.css')  }}
-    {{ HTML::style('assets/utb/css/bootstrap-theme.min.css')  }}
-    {{ HTML::style('assets/utb/css/utb.css')  }}
+    {{ HTML::style('assets/utb/css/bootstrap.min.css')  }}
+    {{ HTML::style('assets/utb/css/bootstrap-theme.min.css') }}
+    {{ HTML::style('assets/utb/css/utb.css') }}
+    {{ HTML::style('assets/customs/css/custom.css') }}
 
     @section('styles')
     @show
 </head>
 <body>
+    @include('layouts.top_menu')
     <!-- El container es la caja principal, allí se ponen todos los contenidos, se recomienda mantener -->
         <div class="container">
 
-            <!-- Menú auxiliar, este menú se usará para todos los sitios, allí van los accesos más comunes, savio, sirius, etc -->
-            <div class="row clearfix">
-                <div class="col-md-12 column">
-                    <nav class="navbar navbar-default navbar-inverse" role="navigation">
-                        <div class="navbar-header">
-                             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                                <span class="sr-only">Toggle navigation</span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                            </button>
-                            {{ link_to('#', 'UTB') }}
-                        </div>
 
-                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                            <ul class="nav navbar-nav navbar-right">
-                                <li>
-                                    {{ link_to('http://bienestar.unitecnologica.edu.co/', 'Bienestar Institucional') }}
-                                </li>
-                                <li>
-                                    {{ link_to('http://www.utbvirtual.edu.co/', 'SAVIO') }}
-                                </li>
-                                <li>
-                                    {{ link_to('#', 'SIRIUS') }}
-                                </li>
-                                <li>
-                                    {{ link_to('http://primo.gsl.com.mx:1701/', 'Bibliotecas') }}
-                                </li>
-                                <li>
-                                   {{ link_to('http://correo.utbvirtual.edu.co/', 'Correo') }}
-                                </li>
-                            </ul>
-                        </div>
-
-                    </nav>
-                </div>
-            </div>
 
             <!-- Área encabezado ppal, Incluye logo, nombre, slogan, buscador y redes sociales -->
             <div class="row clearfix">
