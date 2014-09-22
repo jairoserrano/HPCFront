51bce9688950597eaf2b5867cac5755625ba4c50
Diego Navarro <diego.nava07@gmail.com>
2014-09-22 11:58:42 -0500

    Migraciones nuevas
    Se agregaron las migraciones para Jobs, Entries, Results

---
+++ app/database/migrations/2014_09_22_163224_create_jobs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateJobsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('jobs', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->string('name');
+            $table->enum('type', array('java', 'php', 'bash', 'python'));
+			$table->timestamps();
+
+            $table->integer('project_id')->unsigned()->index();
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('jobs');
+	}
+
+}
---
+++ app/database/migrations/2014_09_22_163247_create_results_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateResultsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('results', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->string('name');
+            $table->text('path');
+
+            $table->integer('job_id')->unsigned()->index();
+            $table->foreign('job_id')->references('id')->on('jobs')->onDelete('cascade');
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('results');
+	}
+
+}
