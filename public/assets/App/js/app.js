cb1937d22582c744c9f50321268881f57ca03e4c
Diego Navarro <diego.nava07@gmail.com>
2014-09-25 11:34:02 -0500

    Actualización de los assets
    Borro cuenta nueva y nueva configuración para dependencias por medio de
    bower.

--- public/assets/App/js/app.js
+++
@@ -1,9 +0,0 @@
-var App = function($, window){
-    return{
-        init: function () {
-            $('#alternative-menu').live('hover', function(){
-                $(this).removeClass('close');
-            });
-        }
-    }
-}(jQuery, window);
