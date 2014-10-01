39a43e1b2bec80130b965a4cbea3f06ec60ce28b
Diego Navarro <diego.nava07@gmail.com>
2014-10-01 10:34:49 -0500

    Actualización de Rutas
    Se agregaron rutas para manejar mejor los jobs y se eliminó la ruta
    resource para los resultados

--- app/routes.php
+++ app/routes.php
@@ -10,13 +10,13 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
-
+Route::get('/', array('uses' => 'ProjectsController@index'));
 Route::get('projects/{id}/create/job', array('as' => 'new_job', 'uses' => 'JobsController@newJob'));
 Route::get('jobs/{id}/create/entry', array('as' => 'new_entry', 'uses' => 'EntriesController@newEntry'));
 Route::get('jobs/{id}/run', array('as' => 'run_job', 'uses' => 'JobsController@runJob'));
+Route::post('jobs/{id}/exec', array('as' => 'exec_job', 'uses' => 'JobsController@executeJob'));
 Route::get('entry/getdocument/{id}', 'EntriesController@getFile');
 Route::get('result/getdocument/{id}', 'ResultsController@getFile');
 Route::resource('entries', 'EntriesController', array('except' => array('create', 'show', 'index')));
 Route::resource('projects', 'ProjectsController');
-Route::resource('jobs', 'JobsController', array('except' => array('create', 'show')));
-Route::resource('results', 'ResultsController', array('only', array('store', 'destroy')));
+Route::resource('jobs', 'JobsController', array('except' => array('create')));
