93728e0879ed38ef2b7ba868313622c60bad18e4
Diego Navarro <diego.nava07@gmail.com>
2014-10-01 11:24:00 -0500

    Nombre de BD en producción
    Cambio a pedido del cliente

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'production',
+			'database'  => 'hpcfront',
 			'username'  => 'root',
 			'password'  => 'root',
 			'charset'   => 'utf8',
