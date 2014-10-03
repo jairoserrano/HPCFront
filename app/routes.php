56a51d9c8b8cbe4b3c4bff71dfcf4a93a8589b37
Diego Navarro <diego.nava07@gmail.com>
2014-10-03 15:27:47 -0500

    Auth de rutas para

--- app/routes.php
+++ app/routes.php
@@ -10,9 +10,13 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
-Route::get('/', array( 'as'=> 'login', 'uses' => 'AuthController@getLogin'));
-Route::post('auth', array( 'as'=> 'auth', 'uses' => 'AuthController@authLogin'));
 
+Route::group(array('before' => 'guest'), function () {
+Route::get('/', array( 'as'=> 'login', 'uses' => 'AuthController@getLogin'));
+Route::post('auth', array( 'as'=> 'auth', 'uses' => 'AuthController@auth'));
+});
+
+Route::group(array('before' => 'auth'), function () {
 Route::get('projects/{id}/create/job', array('as' => 'new_job', 'uses' => 'JobsController@newJob'));
 Route::get('jobs/{id}/create/entry', array('as' => 'new_entry', 'uses' => 'EntriesController@newEntry'));
 Route::get('jobs/{id}/run', array('as' => 'run_job', 'uses' => 'JobsController@runJob'));
@@ -24,3 +28,4 @@ Route::resource('projects', 'ProjectsController');
 Route::resource('jobs', 'JobsController', array('except' => array('create')));
 
 Route::get('logout', array( 'as'=> 'logout', 'uses' => 'AuthController@authLogout'));
+});
