ca666e7e994c17eb90aa8d2cc6abbc7a7f6a275f
Diego Navarro <diego.nava07@gmail.com>
2014-10-06 13:28:14 -0500

    Se agregan librerías locales para el uso de respondJS en el login

--- app/views/auth/login.blade.php
+++ app/views/auth/login.blade.php
@@ -17,8 +17,8 @@
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-        {{ HTML::script(url("https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js")) }}
-        {{ HTML::script(url("https://oss.maxcdn.com/respond/1.4.2/respond.min.js")) }}
+        {{ HTML::script(asset('assets/html5shiv/dist/html5shiv.min.js')) }}
+        {{ HTML::script(asset('assets/respond/dest/respond.min.js')) }}
     <![endif]-->
   </head>
 
