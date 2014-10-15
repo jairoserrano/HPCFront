c60672dbcd73aa0a401fd7ad6de26f9e1d812340
Diego Navarro <diego.nava07@gmail.com>
2014-10-15 16:04:29 -0500

    Se ha arreglado la indexación

--- app/database/migrations/2014_09_18_140121_create_projects_table.php
+++ app/database/migrations/2014_09_18_140121_create_projects_table.php
@@ -3,33 +3,35 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateProjectsTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('projects', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->string('name');
-			$table->text('description');
-			$table->timestamps();
-		});
-	}
+class CreateProjectsTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
 
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('projects');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('projects');
+
+    }
 
 }
