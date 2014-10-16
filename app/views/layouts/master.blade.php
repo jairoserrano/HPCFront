95b5575a4d9a50924a64317f3cd60e731117becc
Diego Navarro <diego.nava07@gmail.com>
2014-10-16 16:27:50 -0500

    Se ha agregado el enlace para salir de aplicación

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -39,14 +39,12 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Project name</a>
+          <a class="navbar-brand" href="#">HPCFront</a>
         </div>
         <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav navbar-right">
             <li><a href="#">{{{ Auth::user()->full_name }}}</a></li>
-            <li><a href="#">Settings</a></li>
-            <li><a href="#">Profile</a></li>
-            <li><a href="#">Help</a></li>
+            <li><a href="{{{ route('logout') }}}">Salir</a></li>
           </ul>
           <form class="navbar-form navbar-right">
             <input type="text" class="form-control" placeholder="Search...">
