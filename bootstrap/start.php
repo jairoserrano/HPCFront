8828d49b28f87c08d24ef98211d712c7fa3fcba0
Diego Navarro <diego.nava07@gmail.com>
2014-10-16 13:23:43 -0500

    Se ha agregado la maquina de Jairo al entorno local

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('homestead', 'perseus-macbook.local'),
+	'local' => array('homestead', 'perseus-macbook.local', 'jairo'),
 	'grid' => array('grid.unitecnologica.edu.co'),
 ));
 
