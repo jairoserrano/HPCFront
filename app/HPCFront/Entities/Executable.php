efa8431976b76af9bfd995a25c667986ca86dd48
Diego Navarro <diego.nava07@gmail.com>
2014-10-27 00:17:43 -0500

    Entidad Excutable
    Ésta nueva entidad permitirá tener de manera sola los ejecutables a
    usar en el server

---
+++ app/HPCFront/Entities/Executable.php
@@ -0,0 +1,26 @@
+<?php namespace HPCFront\Entities;
+
+use Symfony\Component\Finder\SplFileInfo;
+
+class Executable extends BaseEntity implements EntityInterface{
+
+    protected $fillable = array('name', 'path', 'type');
+
+    public function job(){
+        return $this->belongsTo('\HPCFront\Entities\Job');
+    }
+
+    public function getFileNameAttribute(){
+
+        return $this->getFileSpecs($this->path)->getFilename();
+    }
+
+    public function getFileSizeAttribute(){
+
+        return round($this->getFileSpecs($this->path)->getSize() / 1048576, 2);
+    }
+
+    private function getFileSpecs($file_path){
+        return new SplFileInfo($file_path, $file_path, $file_path);
+    }
+} 
