9ff9cad7538e248b1c707b9523612a018ab90ba1
Diego Navarro <diego.nava07@gmail.com>
2014-10-27 00:22:56 -0500

    Actualización de rutas
    Se actualizan las rutas para mejor uso de las rutas enlazadas. Se
    incluye la entidad Executables y sus rutas RESTful

--- app/routes.php
+++ app/routes.php
@@ -18,13 +18,17 @@ Route::group(array('before' => 'guest'), function () {
 
 Route::group(array('before' => 'auth'), function () {
 
+    Route::resource('executables', 'ExecutablesController', array('except' => 'show'));
+    Route::get('executables/download/{id}',array('as'=> 'download_executable', 'uses' => 'ExecutablesController@downloadFile'));
+
+
     Route::resource('projects', 'ProjectsController');
     Route::resource('project.jobs', 'ProjectJobsController', array('except' => 'index'));
     Route::resource('project.job.entries', 'JobEntriesController', array('except' => array('show', 'index')));
     Route::get('entry/get-entry/{entry_id}',array('as'=> 'get_entry', 'uses' => 'JobEntriesController@getFile'));
 
-    Route::get('jobs/{id}/run', array('as' => 'run_job', 'uses' => 'JobsController@runJob'));
-    Route::post('jobs/{id}/exec', array('as' => 'exec_job', 'uses' => 'JobsController@executeJob'));
+    Route::get('jobs/{id}/run', array('as' => 'run_job', 'uses' => 'ProjectJobsController@runJob'));
+    Route::post('jobs/{id}/exec', array('as' => 'exec_job', 'uses' => 'ProjectJobsController@executeJob'));
     Route::get('download/{result}', array('as'=> 'download_result', 'uses' => 'JobsController@downloadResult'));
 
 
